3511b6e4f86fd57223fd61649d1c05ea5741f24f
JoaquinMelendez <jmelendezhenriquez@gmail.com>
2025-05-12 17:22:06 -0400

    Primer Commit

---
+++ src/main/java/com/digital_minds/cl/orden_deparmento/OrdenDeparmentoApplication.java
@@ -0,0 +1,13 @@
+package com.digital_minds.cl.orden_deparmento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenDeparmentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenDeparmentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital_minds/cl/orden_deparmento/OrdenDeparmentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital_minds.cl.orden_deparmento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdenDeparmentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
